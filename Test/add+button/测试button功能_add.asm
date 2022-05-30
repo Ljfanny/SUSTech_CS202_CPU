9e6e863bb280f6b72e5d0fa1359143aaef605eb8
Ljfanny <84827635+Ljfanny@users.noreply.github.com>
2022-05-30 21:54:30 +0800

    add test.

--- Test/add+button/测试button功能_add.asm
+++ Test/add+button/测试button功能_add.asm
@@ -1,29 +1,25 @@
-.data 0x0000				      		
-	buf: .word 0x0000
+.data 0x0000
 .text 0x0000						
 start: lui   $1,0xFFFF			
        ori   $28,$1,0xF000
        addi   $s0, $zero, 0
        addi   $s2, $zero, 1
        addi   $s3, $zero, 2
-# 测试两个数相加 -> 涵盖button功能
-# 0xFFFFFC64 65 66 67       		
+       addi   $s4, $zero, 0x0001FFFF
+# 测试两个数相加 -> 涵盖button功能	
 switled:
-	lw   $t0, 0xC64($28)
-	andi  $t0, $t0, 1
-	beq  $t0, $s2, new_number
+	lw $t0, 0xC70($28)
+	bne $t0, $s4, new_number
 	j switled
 new_number:									
-	lw $t1, 0xC70($28)
-	sw $zero, 0xC64($28)				
+	sw $s4, 0xC70($28)				
 	addi $s0, $s0, 1
 	beq $s0, $s3, cal
-	add $s1, $zero, $t1
+	add $s1, $zero, $t0
 	j switled
 cal:
-	add $t2, $t1, $s1
+	add $t2, $t0, $s1
 	sw  $t2,0xC62($28)
 	addi $s0, $zero, 1
 	j switled
-		
 	
